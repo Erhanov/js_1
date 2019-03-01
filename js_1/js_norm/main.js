75b99b5b79701146d8fdbdda2fdfbc8939fa6116
Azat <aza.er.01@mail.ru>
2019-03-02 00:25:27 +0600

    8

--- js_1/js_norm/main.js
+++ js_1/js_norm/main.js
@@ -1,16 +1,17 @@
-var money = +prompt("Ваш бюджет за месяц?", "");
+var money = +prompt("Ваш бюджет за месяц?", "500000");
 	time = prompt("Введите дату в формате YYYY-MM-DD", ""),
 	expense_question = prompt("Введите обязательную статью расходов?", ""),
-	expense_question1 = prompt("Во сколько обойдется", ""),
-	expense_question = prompt("Введите обязательную статью расходов?", ""),
-	expense_question1 = prompt("Во сколько обойдется", "");
+	expense_question1 = +prompt("Во сколько обойдется", "500"),
+	expense_question2 = prompt("Введите обязательную статью расходов?", ""),
+	expense_question3 = +prompt("Во сколько обойдется", "501");
 
 
 var appData = {
 	budjet: money,
 	timeData: time,
 	expense: {
-		expense_question: expense_question1
+		expense_question: expense_question1,
+		expense_question2: expense_question3
 	},
 	optionalExpences: {},
 	income: [],
@@ -22,3 +23,4 @@ var appData = {
 appData.moneyPerDay = appData.budjet / 30;
 
 alert('everyday earn is ' + appData.moneyPerDay);
+console.log(appData);
